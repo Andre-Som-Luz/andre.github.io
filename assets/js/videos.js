4a53fd62585f13fa28364c032f81ee90e7c33f62
Potestade Desenvolvimento Web <andre@andresomeluz.com.br>
2023-10-29 20:22:25 -0300

    Atualização

--- assets/js/videos.js
+++ assets/js/videos.js
@@ -2,30 +2,31 @@ window.addEventListener('DOMContentLoaded', function () {
   const clickableVideos = document.querySelectorAll('.clickable-video');
   const videos = document.querySelectorAll('.custom-video');
   const thumbnails = document.querySelectorAll('.thumbnail');
-  const logo = document.querySelectorAll('.logo');
+  const overlayImages = document.querySelectorAll('.overlay-image');
 
-  clickableVideos.forEach((clickableVideo) => {
+  clickableVideos.forEach((clickableVideo, index) => {
     const thumbnail = clickableVideo.querySelector('.thumbnail');
-    const customVideo = clickableVideo.querySelector('.custom-video');  
+    const customVideo = clickableVideo.querySelector('.custom-video');
+    const overlayImage = overlayImages[index];
 
     clickableVideo.addEventListener('click', () => {
       videos.forEach((otherVideo) => {
         otherVideo.pause();
         clickableVideos.forEach((c) => c.classList.remove('playing'));
         thumbnails.forEach((tn) => (tn.style.display = 'block'));
-        logo.style.display = 'block';  
+        overlayImages.forEach((img) => (img.style.display = 'none')); // Esconde todas as imagens de sobreposição
       });
 
       clickableVideo.classList.add('playing');
       customVideo.play();
       thumbnail.style.display = 'none';
-      logo.style.display = 'block';
+      overlayImage.style.display = 'block'; // Exibe a imagem de sobreposição
     });
 
     customVideo.addEventListener('ended', () => {
       clickableVideo.classList.remove('playing');
       thumbnail.style.display = 'block';
-      logo.style.display = 'none';
+      overlayImage.style.display = 'none'; // Esconde a imagem de sobreposição quando o vídeo terminar
     });
   });
 });

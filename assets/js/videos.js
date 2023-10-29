98f52bde58a2c3543b31d5ec17611c9eb24db92f
Potestade Desenvolvimento Web <andre@andresomeluz.com.br>
2023-10-29 20:10:35 -0300

    Atualização

--- assets/js/videos.js
+++ assets/js/videos.js
@@ -2,10 +2,11 @@ window.addEventListener('DOMContentLoaded', function () {
   const clickableVideos = document.querySelectorAll('.clickable-video');
   const videos = document.querySelectorAll('.custom-video');
   const thumbnails = document.querySelectorAll('.thumbnail');
+  const logo = document.querySelectorAll('.logo');
 
   clickableVideos.forEach((clickableVideo) => {
     const thumbnail = clickableVideo.querySelector('.thumbnail');
-    const customVideo = clickableVideo.querySelector('.custom-video');
+    const customVideo = clickableVideo.querySelector('.custom-video');  
 
     clickableVideo.addEventListener('click', () => {
       videos.forEach((otherVideo) => {
@@ -17,11 +18,13 @@ window.addEventListener('DOMContentLoaded', function () {
       clickableVideo.classList.add('playing');
       customVideo.play();
       thumbnail.style.display = 'none';
+      logo.style.display = 'block';
     });
 
     customVideo.addEventListener('ended', () => {
       clickableVideo.classList.remove('playing');
       thumbnail.style.display = 'block';
+      logo.style.display = 'none';
     });
   });
 });
